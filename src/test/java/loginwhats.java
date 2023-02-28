6b47f69f738d4707c9d43a6e819e6dfd6e1fff42
mmousa91 <109099345+mmousa91@users.noreply.github.com>
2023-02-28 17:11:30 +0200

    final

--- src/test/java/loginwhats.java
+++ src/test/java/loginwhats.java
@@ -3,12 +3,16 @@ import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import java.io.*;
 
@@ -16,26 +20,31 @@ import java.io.*;
 public class loginwhats {
 
     WebDriver driver = null;
+    Actions action ;
 
-    @Test
-    public void openBrowser() throws InterruptedException, IOException {
 
+
+    @BeforeTest
+    public void open (){
         String chromePath = System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe";
         System.setProperty("WebDriver.chrome.driver", chromePath);
 
         driver = new ChromeDriver();
 
         driver.manage().window().maximize();
+    }
 
-//1- go to http://live.techpanda.org/index.php/
+    @Test(priority = 1)
+    public void openBrowser() throws InterruptedException, IOException {
+
         //driver.navigate().to("http://justnotepad.com/");
-        driver.navigate().to("http://justnotepad.com/get/a1609a9f4bcf420ed89d7935a6314b40");
+        driver.navigate().to("http://justnotepad.com/get/807dc12dcf67017f28d958103e02ee25");
         Thread.sleep(3000);
 
         driver.findElement(By.id("password")).sendKeys("12345678");
         driver.findElement(By.id("password")).sendKeys(Keys.ENTER);
         Thread.sleep(2000);
-        Actions action = new Actions(driver);
+         action = new Actions(driver);
         driver.findElement(By.id("editable_text")).click();
         action.keyDown(Keys.CONTROL);
         action.sendKeys("a");
@@ -49,105 +58,69 @@ public class loginwhats {
 
         driver.switchTo().newWindow(WindowType.TAB);
         driver.navigate().to("https://web.whatsapp.com/");
-        /*driver.findElement(By.id("editable_text")).click();
-        action.keyDown(Keys.CONTROL);
-        action.sendKeys("v");
-        action.keyUp(Keys.CONTROL);
-        action.build().perform();*/
+        Thread.sleep(20000);
 
-        Thread.sleep(40000);
-       /*
-       //
-        File inFile = new File("C:\\Users\\MMousa\\Downloads\\sale.txt");
-        StringBuilder targetString = new StringBuilder("");
-        try {
-            FileReader fr = new FileReader(inFile);
-            BufferedReader br = new BufferedReader(fr);
+    }
 
-            String s = null;
-            while ((s = br.readLine()) != null) {
-                targetString.append(s);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //*/
+@Test (priority = 2)
+    public void  iteration () throws InterruptedException, IOException {
+    File file = new File("C:\\Users\\MMousa\\Downloads\\num.xlsx");
 
-        File file = new File( "C:\\Users\\moham\\Downloads\\num.xlsx");
+    //Create an object of FileInputStream class to read excel file
+    FileInputStream inputStream = new FileInputStream(file);
 
-        //Create an object of FileInputStream class to read excel file
-        FileInputStream inputStream = new FileInputStream( file);
+    //Creating workbook instance that refers to .xlxs file
+    XSSFWorkbook wb = new XSSFWorkbook(inputStream);
 
-        //Creating workbook instance that refers to .xlxs file
-        XSSFWorkbook wb = new XSSFWorkbook(inputStream);
+    //Creating a Sheet object using the sheet Name
+    XSSFSheet sheet = wb.getSheet("phones");
+    //  XSSFSheet sheetMsg = wb.getSheet("Msg");
 
-        //Creating a Sheet object using the sheet Name
-        XSSFSheet sheet = wb.getSheet("phones");
-        //  XSSFSheet sheetMsg = wb.getSheet("Msg");
+    //Create a row object to retrieve row at index 1
+    XSSFRow row1 = sheet.getRow(0);
+    // XSSFRow rowMsg = sheetMsg.getRow(0);
 
-        //Create a row object to retrieve row at index 1
-        XSSFRow row1 = sheet.getRow(0);
-        // XSSFRow rowMsg = sheetMsg.getRow(0);
+    //Create a cell object to retreive cell at index 0
+    XSSFCell cell1 = row1.getCell(0);
+    // XSSFCell cellMsg = rowMsg.getCell(0);
 
-        //Create a cell object to retreive cell at index 0
-        XSSFCell cell1 = row1.getCell(0);
-        // XSSFCell cellMsg = rowMsg.getCell(0);
+    //Get the phone in a variable
+    //String phoneNum = cell1.getStringCellValue();
+    int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+    for (int i = 0; i <= rowCount; i++)
+    {
+        //get cell count in a row
+        int cellcount = sheet.getRow(i).getLastCellNum();
 
-        //Get the phone in a variable
-        String phoneNum = cell1.getStringCellValue();
-        //  System.out.println(phoneNum);
+        for (int j = 0; j < cellcount; j++) {
+            driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).click();
+            driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).clear();
+            driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).sendKeys(sheet.getRow(i).getCell(j).getStringCellValue());
+            driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).sendKeys(Keys.ENTER);
+            action.keyDown(Keys.CONTROL);
+            action.sendKeys("v");
+            action.keyUp(Keys.CONTROL);
+            action.build().perform();
+Thread.sleep(5000);
+            action.sendKeys(Keys.ENTER);
+            action.build().perform();
+            Thread.sleep(5000);
 
-        //get the numbers of rows in the sheet
-        int rowCount = sheet.getLastRowNum()-sheet.getFirstRowNum();
-        //System.out.println("the num of rows is : " + rowCount);
-
-        for(int i=0;i<=rowCount;i++){
-
-            //get cell count in a row
-            int cellcount=sheet.getRow(i).getLastCellNum();
-
-            //iterate over each cell to print its value
-            // System.out.println("Row"+ i+" data is :");
-
-            for(int j=0;j<cellcount;j++){
-
-                driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).clear();
-                driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).click();
-                driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).sendKeys(sheet.getRow(i).getCell(j).getStringCellValue());
-              //  System.out.println(driver.findElement(By.xpath("//*[@id=\"pane-side\"]/div[1]/div/span")).getText());
-                if (driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).isDisplayed()) {
-                    driver.findElement(By.xpath("//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[2]")).sendKeys(Keys.ENTER);
-                    driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")).click();
-                    driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")).clear();
-
-                    action.keyDown(Keys.CONTROL);
-                    action.sendKeys("v");
-                    action.keyUp(Keys.CONTROL);
-                    action.build().perform();
-                    driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p")).
-                            sendKeys(Keys.ENTER);
-                    Thread.sleep(2000);
-
-                }
-                    else {
-                    openBrowser();
-                }
-
-            }
+           //driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]")).sendKeys(Keys.ENTER);
+            action.sendKeys(Keys.ESCAPE);
+            action.build().perform();
+            Thread.sleep(6000);
 
         }
 
-
-        Thread.sleep(3000);
-        driver.quit();
     }
+}
 
-    /*
-//
-    @AfterTest
-    public  void closeBrowser () throws InterruptedException {
-        Thread.sleep(3000);
+                @AfterTest
+    public void closeBrowser () throws InterruptedException
+                {
+
+        Thread.sleep(10000);
         driver.quit();
-    }
-*/
+            }
 }
